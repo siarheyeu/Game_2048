fab937669848dfd13b22d8794b4201e3be4338d5
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-06-27 23:57:26 +0300

    Сhanging the size of the playing field

--- src/Game2048.java
+++ src/Game2048.java
@@ -2,6 +2,15 @@ package com.javarush.games.game2048;
 
 import com.javarush.engine.cell.*;
 
-public class Game2048 extends Game {
+import javax.print.attribute.standard.MediaTray;
 
+
+
+public class Game2048 extends Game {
+    public static final int SIDE = 4;
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }
